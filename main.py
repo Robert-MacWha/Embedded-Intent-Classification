56ed5e5de368b9c223ba92da2c26978c14bdd85d
Robert MacWha <trebor.ahwcam@gmail.com>
2022-04-03 08:26:34 -0400

    Updated with sample image

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from Aurras import Aurras
 aurras = Aurras()
 aurras.load()
 
-print("\nPositive Similarity, known")
+print("\nPositive Similarity, known intents")
 aurras.get_intent("What do I have to do today?", "What reminders do I have?")
 
-print("\nNegative Similarity, known")
+print("\nNegative Similarity, known intents")
 aurras.get_intent("What do I have to do today?", "Play some Jazz")
 
-print("\nPositive Similarity, unknown")
+print("\nPositive Similarity, unknown intents")
 aurras.get_intent("Book me an uber to downtown", "Please setup an uber to get to school")
 
-print("\nPositive Similarity, unknown")
-aurras.get_intent("Start playing a short hike", "Open up cities skylines on steam")
-
-print("\nNegative Similarity, unknown")
-aurras.get_intent("Book me an uber to downtown", "Open up cities skylines on steam")
+print("\nNegative Similarity, unknown intents")
+aurras.get_intent("Open visual studio code", "Text Jason and tell him that I'll be a little late")
